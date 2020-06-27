378601dea5592c3bf1455a233c37ca947cfb4a8d
Aditya Sharma <iit2017019@iiita.ac.in>
2020-06-28 02:28:52 +0530

    adding June_LT20 Folder

---
+++ June_LT20/Q1.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <chrono>
+#include <complex>
+#define int long long int
+#define pb push_back
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define all(v) v.begin(),v.end()
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define ff first
+#define ss second
+#define el endl
+#define MAX 100005
+#define wl while
+#define all(v) v.begin(),v.end()
+#define rep(i,n) for (int i = 0; i < n; i++)
+#define rep_1(i,n) for (int i = 1; i <= n; i++)
+#define rep_it(it,mp) for(auto it=mp.begin();it!=mp.end();it++)
+#define FOR(i,a,b) for (int i = a; i < b; i++)
+#define REPD(i,n) for (int i = n-1; i >= 0; i--)
+using namespace std;
+
+
+signed main(){
+#ifndef ONLINE_JUDGE
+  freopen("input.txt","r", stdin);
+  freopen("output.txt","w", stdout);
+#endif
+
+  ios::sync_with_stdio(false);
+  cin.tie(0), cout.tie(0);
+
+  int t;
+  cin>>t;
+  wl(t--){
+    int s,n;
+    cin>>s>>n;
+    if(s<=n){
+      if(s&1){
+        if(s==1){
+          cout<<1<<el;
+        }
+        else{
+          cout<<2<<el;
+        }
+      }
+      else{
+        cout<<1<<el;
+      }
+    }
+    else{
+      int ans=0;
+      if(s&1){
+        s--;
+        ans++;
+      }
+      int d = s/n;
+      ans+=d;
+      s-=(d*n);
+      if(s!=0){
+        if(s&1){
+          ans+=2;
+        }
+        else{
+          ans+=1;
+        }
+      }
+      cout<<ans<<el;
+    }
+  }
+
+  return 0;
+}
