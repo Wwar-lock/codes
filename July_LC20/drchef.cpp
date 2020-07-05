e5f1df489a89cd5958a089070790625874324e14
Aditya Sharma <iit2017019@iiita.ac.in>
2020-07-06 03:21:35 +0530

    hint added

--- July_LC20/drchef.cpp
+++ July_LC20/drchef.cpp
@@ -67,11 +67,20 @@ signed main(){
     }
     else{
       int cnt=0;
-      int d=0;
-      multiset<int>st;
-      rep(i,n){st.insert(v[i]);}
-      sort(all(v));
-      
+      vi l,g;
+      rep(i,n){
+        if(v[i]<x){l.pb(v[i]);}
+        else{g.pb(v[i]);}
+      }
+      sort(all(g));
+      // printV(g);
+      sort(all(l),greater<int>());
+      int counter=0;
+      // 1.priority_queue implementation for greater values of x :
+      // printV(l);
+      // cout<<cnt<<el;
+      cnt+=l.size();
+      cout<<cnt<<el;
     }
 
   }
