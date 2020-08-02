64f0d9df7df9ca98389186557a64060083efcc8d
Aditya Sharma <iit2017019@iiita.ac.in>
2020-08-02 15:08:03 +0530

    checking

---
+++ codeMantra/sample.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <chrono>
+#include <complex>
+#define int long long int
+#define pb push_back
+#define vt vector
+#define all(v) v.begin(),v.end()
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define ff first
+#define ss second
+#define el endl
+#define MAX 100005
+#define wl while
+#define umap unordered_map
+#define uset unordered_set
+#define all(v) v.begin(),v.end()
+#define rep(i,n) for (int i = 0; i < n; i++)
+#define rep_1(i,n) for (int i = 1; i <= n; i++)
+#define rep_it(it,mp) for(auto it=mp.begin();it!=mp.end();it++)
+#define FOR(i,a,b) for (int i = a; i < b; i++)
+#define REPD(i,n) for (int i = n-1; i >= 0; i--)
+using namespace std;
+
+
+signed main(){
+#ifndef ONLINE_JUDGE
+  freopen("input.txt","r", stdin);
+  freopen("output.txt","w", stdout);
+#endif
+
+  ios::sync_with_stdio(false);
+  cin.tie(0), cout.tie(0);
+   int k;cin>>k;
+  string s;
+  cin>>s;
+ 
+    int n = s.size();
+    string ans;
+    int cnt=0;
+    int i;
+    for(i=0;i<n-1;i++){
+        if(cnt==k){
+            break;
+        }
+        else if(s[i]>s[i+1]){
+            ans+=s[i];  
+        }
+        else{
+            cnt++;
+        }
+    }
+    if(cnt>=k){
+      ans+=s.substr(i,n);
+    }
+    cout<<ans<<el;
+
+  return 0;
+}
