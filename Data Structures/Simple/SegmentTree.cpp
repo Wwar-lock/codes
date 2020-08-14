46b42b1a69e82ce2fcc9d7f8d9f6b7d33dd85f97
Aditya Sharma <iit2017019@iiita.ac.in>
2020-08-14 10:58:23 +0530

    Update SegmentTree.cpp

--- Data Structures/Simple/SegmentTree.cpp
+++ Data Structures/Simple/SegmentTree.cpp
@@ -34,92 +34,32 @@ using namespace std;
 // updation complexity is : O(log(n))
 // query complexity is : O(log(n))
 
-int getMid(int i,int j){
-  return i+(j-i)/2;
+const int N = 1e5;  // limit for array size
+int n;  // array size
+int t[2 * N];
+
+void build() {  // build the tree
+  for (int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
 }
 
-int constructUtilFunction(vi v,int ss,int se,int *st,int si){
-  if(v[ss]==v[se]){
-    st[si]=v[ss];
-    return v[ss];
-  }
-  int mid = getMid(ss,se);
-  st[si] = constructUtilFunction(v,ss,mid,st,2*si+1)+constructUtilFunction(v,mid+1,se,st,2*si+2);
-  return st[si];
+void modify(int p, int value) {  // set value at position p
+  for (t[p += n] = value; p > 1; p >>= 1) t[p>>1] = t[p] + t[p^1];
 }
 
-int *contructSegmentTree(vi v){
-  int h = (int)(ceil(log2(v.size())));
-  int mx_size  = 2*pow(2,h)-1;
-  int *st = new int[mx_size];
-  constructUtilFunction(v,0,v.size()-1,st,0);
-  return st;
+int query(int l, int r) {  // sum on interval [l, r)
+  int res = 0;
+  for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+    if (l&1) res += t[l++];
+    if (r&1) res += t[--r];
+  }
+  return res;
 }
 
-void updateUtilFunction(int *st,int ss,int se,int id,int diff,int si){
-  if(id<ss||id>se){
-    return;
-  }
-  st[si]+=diff;
-  if(ss!=se){
-    int mid = getMid(ss,se);
-    updateUtilFunction(st,ss,mid,id,diff,2*si+1);
-    updateUtilFunction(st,ss,mid,id,diff,2*si+2);
-  }
-}
-
-void updateValue(vi v,int *st,int size,int id,int val){
-  if(id<0||id>size-1){
-    cout<<"Invalid";
-    return;
-  }
-  int diff = val-v[id];
-  v[id] = val;
-  updateUtilFunction(st,0,size-1,id,diff,0);
-}
-
-int getSumUtil(int *st,int ss,int se,int qs,int qe,int si){
-  if(qs<=ss&&qe>=se){
-    return st[si];
-  }
-  if(se<qs||ss>qe){
-    return 0;
-  }
-  int mid = getMid(ss,se);
-  return getSumUtil(st,ss,mid,qs,qe,2*si+1)+getSumUtil(st,mid+1,se,qs,qe,2*si+2);
-}
-
-int getSum(int *st,int qs,int qe,int n){
-  if(qs<0||qs>n-1||qe<0||qe>n-1){
-    return -1e7;
-  }
-  return getSumUtil(st,0,n-1,qs,qe,0);
-}
-
-signed main(){
-#ifndef ONLINE_JUDGE
-  freopen("input.txt","r", stdin);
-  freopen("output.txt","w", stdout);
-#endif
-
-  ios::sync_with_stdio(false);
-  cin.tie(0), cout.tie(0);
-
-  // vi v;
-  int n;
-  cin>>n;
-  vi v(n);
-  rep(i,n){
-    cin>>v[i];
-  }
-
-  int *st = contructSegmentTree(v);
-
-  int sum = getSum(st,2,4,v.size());
-  cout<<"sum (2,4): "<<sum<<el;
-
-  updateValue(v,st,v.size(),2,9);
-  cout<<getSum(st,2,4,v.size())<<el;
-
+int main() {
+  scanf("%d", &n);
+  for (int i = 0; i < n; ++i) scanf("%d", t + n + i);
+  build();
+  modify(0, 1);
+  printf("%d\n", query(3, 11));
   return 0;
 }
